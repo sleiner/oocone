34a117e3ffa3ed44d8f2e5b0f87963974b8771f3
Simon Leiner <simon@leiner.me>
2025-02-17 00:22:28 +0100

    nox: Improve caching

--- noxfile.py
+++ noxfile.py
@@ -2,6 +2,7 @@ import os
 
 import nox
 
+nox.options.reuse_venv = "yes"
 os.environ.update(PDM_IGNORE_SAVED_PYTHON="1", PDM_USE_VENV="1")
 
 
@@ -9,7 +10,9 @@ def _install(session: nox.Session, *, groups: list[str] | None = None) -> None:
     if groups is None:
         groups = []
 
-    session.run("pdm", "install", *(f"--group={group}" for group in groups), external=True)
+    session.run_install(
+        "pdm", "install", *(f"--group={group}" for group in groups), external=True, silent=True
+    )
 
 
 @nox.session(python=("3.12", "3.13"))
