c7edf32f36d5207c0c3895bc1afb95e8c7c667e6
Simon Leiner <simon@leiner.me>
2025-12-07 13:01:18 +0100

    Use syrupy for snapshot tests

--- tests/enocoo/test_get_areas.py
+++ tests/enocoo/test_get_areas.py
@@ -1,54 +1,23 @@
 """Tests for the oocone.Enocoo.get_area_ids() method."""
 
-import datetime as dt
-
 import pytest
+from syrupy.assertion import SnapshotAssertion
 
 from oocone import Auth, Enocoo
-from oocone.model import Area
 from tests import TIMEZONE
 from tests.conftest import MockApiParams
 
 
 @pytest.mark.parametrize(
-    ("dataset", "expected_areas"),
-    [
-        (
-            "one_area",
-            [
-                Area(
-                    name="H12W34",
-                    id="123",
-                    data_available_since=dt.date(2021, 1, 1),
-                    data_available_until=dt.date(2022, 2, 2),
-                )
-            ],
-        ),
-        (
-            "two_areas",
-            [
-                Area(
-                    name="H12W34",
-                    id="123",
-                    data_available_since=dt.date(2021, 1, 1),
-                    data_available_until=dt.date(2021, 1, 1),
-                ),
-                Area(
-                    name="SP567 TNr:890",
-                    id="124",
-                    data_available_since=dt.date(2021, 1, 1),
-                    data_available_until=dt.date(2021, 1, 1),
-                ),
-            ],
-        ),
-    ],
+    "dataset",
+    ["one_area", "two_areas"],
 )
 async def test_happy_path(
-    dataset: str, expected_areas: list[Area], mock_auth: Auth, mock_api_params: MockApiParams
+    dataset: str, snapshot: SnapshotAssertion, mock_auth: Auth, mock_api_params: MockApiParams
 ) -> None:
     """Check that enocoo.get_areas returns the areas as indicated in ownConsumption.php."""
     mock_api_params.dataset_name = dataset
     enocoo = Enocoo(mock_auth, TIMEZONE)
 
     areas = await enocoo.get_areas()
-    assert areas == expected_areas
+    assert areas == snapshot
